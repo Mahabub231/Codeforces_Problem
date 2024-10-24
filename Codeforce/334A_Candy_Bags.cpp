6a4c9276a35fdc46f981fdb690bfa7db33078092
MD Mahabub Hasan Mahin <mahabubhassan678@gmail.com>
2024-10-24 22:10:25 +0600

    Add files via upload
    Signed-off-by: MD Mahabub Hasan Mahin <mahabubhassan678@gmail.com>

---
+++ Codeforce/334A_Candy_Bags.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; ++i)
+    {
+        cout<<i + 1<<" ";
+        for (int j = 1; j < n; ++j)
+        {
+            cout<<(n * j + (i + j) % n + 1);
+             if (j < n - 1) {
+                cout << " ";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
